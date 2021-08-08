2cb285e98b0068899d9dbdece6c3381a86f8fe5c
bigsaputro <galihisaputro@gmail.com>
2021-08-08 16:53:27 +0700

    lets learn fast api

---
+++ main.py
@@ -0,0 +1,14 @@
+from fastapi import FastAPI
+
+
+app = FastAPI()
+
+
+@app.get('/')
+def index():
+    return {'data': {'name': 'nicolas'}}
+
+
+@app.get('/about')
+def about():
+    return {'data': 'about page'}
